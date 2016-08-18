6e3d1ed95b26aa633ba0cfd9b6e257a6b2da3106
Vítor Almeida Subhi <vitorsubhi@gmail.com>
2016-08-18 17:49:40 -0300

    Add files via upload

--- Accounts.h
+++ Accounts.h
@@ -15,56 +15,52 @@ int login(char user[20], char pass[30])
 	strcat (local, user);
 	fp = fopen (local, "r");
 	if (fp == NULL)
-	{
 		printf ( "Username or/and Password is/are Incorrect.\n");
-	}
 	else
 	{
 		while (fgets(checkpass, 20, fp) != NULL)
 		{
+
 		}
 		fscanf (fp, "%s", pass);
 		if (strcmp(checkpass, pass) != 0)
-		{
 			printf ("Username or/and Password is/are Incorrect.\n");
-		}
 		else
-		{
-			return 1;
-		}
+			return TRUE;
 	}
 }
 
-int newuser(char user[20], char pass[30])
+int checkuser(char user[20])
 {
 	FILE *fp;
 	char local[50]="Files/Users/";
         strcat (local, user);
 	fp = fopen (local, "r");
 	if (fp == NULL)
+		return FALSE;
+	return TRUE;
+}
+
+int newuser(char user[20], char pass[30])
+{
+	FILE *fp;
+	char local[50]="Files/Users/";
+	strcat (local, user);
+	if (!checkuser(user))
 	{
 		fp = fopen (local, "w");
 		fprintf (fp, "%s", pass);
 		printf ("Your Account has been created successfully\n");
 	}
 	else
-	{
 		printf ("Account already exists, try another Username\n");
-	}
 //fclose (fp);
 }
 
 int deluser(char user[20], char pass[30])
 {
-	FILE *fp;
-        char local[50]="Files/Users/";
-	int status;
-        strcat (local, user);
-        fp = fopen (local, "r");
-        if (fp == NULL)
-        {
+	if (!checkuser (user))
                 printf ("This Account does not exists.\n");
-        }
         else
         {
 		while (fgets(checkpass, 20, fp) != NULL)
@@ -72,20 +68,15 @@ int deluser(char user[20], char pass[30])
                 }
                 fscanf (fp, "%s", pass);
                 if (strcmp(checkpass, pass) != 0)
-                {
                         printf ("Username or/and Password is/are Incorrect.\n");
-                }
 		else
 		{
-                	status = remove(local);
+			int status = 1;
+        		status = remove(local);
 			if (status == 0)
-			{
 				printf ("User: %s has been deleted successfully\n", user);
-			}
 			else
-			{
 				printf ("Unable to delete User: %s . Try again Later.\n", user);
-			}
 		}
         }
 
@@ -97,10 +88,8 @@ int edituser(char user[20], char pass[30], char newpass[30])
 	char local[50]="Files/Users/";
 	strcat (local, user);
 	fp = fopen (local, "r");
-	if (fp == NULL)
-	{
+	if (!checkuser(user))
 		printf ("This Account does not exists\n");
-	}
 	else
 	{
 		while (fgets(checkpass, 20, fp) != NULL)
@@ -108,15 +97,11 @@ int edituser(char user[20], char pass[30], char newpass[30])
                 }
                 fscanf (fp, "%s", pass);
                 if (strcmp(checkpass, pass) != 0)
-                {
                         printf ("Username or/and Password is/are Incorrect.\n");
-                }
                 else
                 {
 			if (strcmp(newpass, pass) == 0)
-			{
 				printf ("New Password is the same as the Old Password\n");
-			}
                         else
 			{
 				fp = fopen (local, "w");
